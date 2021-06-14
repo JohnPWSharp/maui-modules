9d661682ade557691212d0934e767fed390a82b5
John Sharp <johns@contentmaster.com>
2021-06-14 11:43:24 +0100

    updates

---
+++ Consume-REST-services/src/exercise1/maui-final/PartsClient/PartsClient/PartsClient/Data/PartsManager.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartsClient.Data
+{
+    public class PartsManager
+    {
+        static readonly string BaseAddress = "{Url from before}";
+        static readonly string Url = $"{BaseAddress}/api/parts/";
+        private string authorizationKey;
+
+        private async Task<HttpClient> GetClient()
+        {
+            HttpClient client = new HttpClient();
+            if (string.IsNullOrEmpty(authorizationKey))
+            {
+                authorizationKey = await client.GetStringAsync(Url + "/login");
+                authorizationKey = JsonConvert.DeserializeObject<string>(authorizationKey);
+            }
+
+            client.DefaultRequestHeaders.Add("Authorization", authorizationKey);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            return client;
+        }
+
+        public async Task<IEnumerable<Part>> GetAll()
+        {
+            HttpClient client = await GetClient();
+            string result = await client.GetStringAsync(Url);
+            return JsonConvert.DeserializeObject<IEnumerable<Part>>(result);
+        }
+
+        public async Task<Part> Add(string partName, string supplier, string partType)
+        {
+            Part part = new Part()
+            {
+                PartName = partName,
+                Suppliers = new List<string>(new[] { supplier }),
+                PartID = string.Empty,
+                PartType = partType,
+                PartAvailableDate = DateTime.Now.Date,
+            };
+
+            HttpClient client = await GetClient();
+            var response = await client.PostAsync(Url,
+                new StringContent(
+                    JsonConvert.SerializeObject(part),
+                    Encoding.UTF8, "application/json"));
+
+            return JsonConvert.DeserializeObject<Part>(
+                await response.Content.ReadAsStringAsync());
+        }
+
+        public async Task Update(Part part)
+        {
+            HttpClient client = await GetClient();
+            await client.PutAsync(Url + "/" + part.PartID,
+                new StringContent(
+                    JsonConvert.SerializeObject(part),
+                    Encoding.UTF8, "application/json"));
+        }
+
+        public async Task Delete(string partID)
+        {
+            HttpClient client = await GetClient();
+            await client.DeleteAsync(Url + partID);
+        }
+    }
+}
---
+++ Consume-REST-services/src/exercise1/maui-final/PartsClient/PartsClient/PartsClient/MainPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using PartsClient.Data;
+
+namespace PartsClient
+{
+    public partial class MainPage : ContentPage, IPage
+    {
+        readonly IList<Part> parts = new ObservableCollection<Part>();
+        readonly PartsManager manager = new PartsManager();
+
+        public MainPage()
+        {
+            BindingContext = parts;
+            InitializeComponent();
+        }
+
+        async void OnRefresh(object sender, EventArgs e)
+        {
+            if (IsBusy)
+                return;
+
+            try
+            {
+                IsBusy = true;
+                var partsCollection = await manager.GetAll();
+                foreach (Part part in partsCollection)
+                {
+                    if (parts.All(p => p.PartID != part.PartID))
+                        parts.Add(part);
+                }
+            }
+            catch (Exception ex)
+            {
+                await this.DisplayAlert("Error",
+                        ex.Message,
+                        "OK");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        async void OnAddNewPart(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(
+                new AddEditPartsPage(manager, parts));
+        }
+
+        async void OnEditPart(object sender, ItemTappedEventArgs e)
+        {
+            await Navigation.PushModalAsync(
+                new AddEditPartsPage(manager, parts, (Part)e.Item));
+        }
+
+        async void OnDeletePart(object sender, EventArgs e)
+        {
+            var item = (MenuItem)sender;
+            var part = item.CommandParameter as Part;
+            if (part == null || IsBusy)
+                return;
+
+            if (await this.DisplayAlert("Delete Part?",
+                "Are you sure you want to delete the part '"
+                    + part.PartName + "'?", "Yes", "Cancel") == true)
+            {
+                try
+                {
+                    IsBusy = true;
+                    await manager.Delete(part.PartID);
+                    parts.Remove(part);
+                }
+                catch (Exception ex)
+                {
+                    await this.DisplayAlert("Error",
+                            ex.Message,
+                            "OK");
+                }
+                finally
+                {
+                    IsBusy = false;
+                }
+            }
+        }
+    }
+}

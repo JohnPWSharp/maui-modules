ce8c0ef34208bc2e5a7b7d7db0ed4e0a52b555e0
John Sharp <johns@contentmaster.com>
2021-10-18 14:25:54 +0100

    Updated for Preview 9

---
+++ Build-mobile-desktop-apps/exercise1/final/Phoneword/Phoneword/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace Phoneword
+{
+	public partial class MainPage : ContentPage
+	{
+		int count = 0;
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void OnCounterClicked(object sender, EventArgs e)
+		{
+			count += 5;
+			CounterLabel.Text = $"Current count: {count}";
+
+			SemanticScreenReader.Announce(CounterLabel.Text);
+		}
+	}
+}
---
+++ Build-mobile-desktop-apps/exercise2/final/Phoneword/Phoneword/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace Phoneword
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        string translatedNumber;
+
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = PhoneNumberText.Text;
+            translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
+
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                CallButton.IsEnabled = true;
+                CallButton.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                CallButton.IsEnabled = false;
+                CallButton.Text = "Call";
+            }
+        }
+
+        async void OnCall(object sender, System.EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                "Dial a Number",
+                "Would you like to call " + translatedNumber + "?",
+                "Yes",
+                "No"))
+            {
+                try
+                {
+                    PhoneDialer.Open(translatedNumber);
+                }
+                catch (ArgumentNullException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+                }
+                catch (FeatureNotSupportedException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone dialing not supported.", "OK");
+                }
+                catch (Exception)
+                {
+                    // Other error has occurred.
+                    await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+                }
+            }
+        }
+    }
+}

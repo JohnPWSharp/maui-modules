969a8f1b5d2e6d4147c6dc956c6480e6f23419f7
John Sharp <johns@contentmaster.com>
2021-10-19 16:46:14 +0100

    Updated for Preview 9

---
+++ Customize-MAUI-layouts/exercise1/start/Alignment/Alignment/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace Alignment
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        void OnHorizontalStartClicked(object sender, EventArgs e) { target.HorizontalOptions = LayoutOptions.Start; }
+        void OnHorizontalCenterClicked(object sender, EventArgs e) { target.HorizontalOptions = LayoutOptions.Center; }
+        void OnHorizontalEndClicked(object sender, EventArgs e) { target.HorizontalOptions = LayoutOptions.End; }
+        void OnHorizontalFillClicked(object sender, EventArgs e) { target.HorizontalOptions = LayoutOptions.Fill; }
+
+        void OnVerticalStartClicked(object sender, EventArgs e) { target.VerticalOptions = LayoutOptions.Start; }
+        void OnVerticalCenterClicked(object sender, EventArgs e) { target.VerticalOptions = LayoutOptions.Center; }
+        void OnVerticalEndClicked(object sender, EventArgs e) { target.VerticalOptions = LayoutOptions.End; }
+        void OnVerticalFillClicked(object sender, EventArgs e) { target.VerticalOptions = LayoutOptions.Fill; }
+    }
+}

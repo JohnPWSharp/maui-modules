0b14bbefdd9f4d6d929d8032070845b0e02ea7c4
John Sharp <johns@contentmaster.com>
2021-10-22 12:29:43 +0100

    Completed and updated for Preview 9

---
+++ Consume-REST-services/exercise1/final/PartsClient/PartsClient/Pages/AddPartPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Maui.Controls;
+using PartsClient.Data;
+using System;
+using System.Collections.Generic;
+
+namespace PartsClient.Pages
+{
+    public partial class AddPartPage : ContentPage
+    {
+        public AddPartPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnAddPart(object sender, EventArgs e)
+        {
+            
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                Part part = new Part
+                {
+                    PartName = PartNameField.Text,
+                    PartType = PartTypeField.Text,
+                    Suppliers = new List<String> { PartSupplierField.Text }
+                };
+
+                var insertedPart = await PartsPage.partsViewModel.AddPart(part);
+                await this.DisplayAlert("Saved",
+                        "Changes saved",
+                        "OK");
+
+                BindingContext = insertedPart;
+            }
+            catch (Exception ex)
+            {
+                await this.DisplayAlert("Error",
+                        ex.Message,
+                        "OK");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        private async void OnClearPart(object sender, EventArgs e)
+        {
+            BindingContext = null;
+            PartIDField.Text = "";
+            PartNameField.Text = "";
+            PartTypeField.Text = "";
+            PartDateAvailableField.Text = "";
+            PartSupplierField.Text = "";           
+        }
+    }
+}
---
+++ Consume-REST-services/exercise1/start/PartsClient/PartsClient/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
+using PartsClient.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PartsClient
+{
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				});
+
+			builder.Services.AddSingleton<AppShell>();
+
+			return builder.Build();
+		}
+	}
+}

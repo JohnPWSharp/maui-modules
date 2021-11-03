a02e04aad3413114dace13e08a98681398464ccd
John Sharp <johns@contentmaster.com>
2021-11-03 17:29:34 +0000

    Code for Exercise 2 complete

---
+++ Use-tabbed-pages/exercise3/start/Astronomy/Astronomy/Data/LatLongService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Maui.Essentials;
+
+namespace Astronomy
+{
+    public class LatLongService : ILatLongService
+    {
+        public async Task<(double Latitude, double Longitude)> GetLatLong()
+        {
+            var latLoc = 0.0;
+            var longLoc = 0.0;
+
+            var status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            if (status == PermissionStatus.Granted)
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Default, TimeSpan.FromSeconds(10));
+                var location = await Geolocation.GetLocationAsync(request);
+                latLoc = location.Latitude;
+                longLoc = location.Longitude;
+            }
+            return (latLoc, longLoc);
+        }
+    }
+}
